e012d71cb01b224eff0084ddb4f36e92690e70f3
manjug <manjug@192.168.29.45>
2022-10-02 17:00:26 +0530

    First Time Trying to link my project from git to eclipse

---
+++ azuga/src/test/java/browserTest/TestingLaunch.java
@@ -0,0 +1,19 @@
+package browserTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestingLaunch {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+		
+		WebDriver driver=new ChromeDriver();
+		System.out.println("ChromeBrowserLaunching");
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.close();
+		
+		
+	}
+
+}
