e869a7821727f10ae1624ad122479a99c54a3110
manjuan90 <107313784+manjuan90@users.noreply.github.com>
2023-04-02 12:25:00 +0530

    Update TestingLaunch.java

--- azuga/src/test/java/browserTest/TestingLaunch.java
+++ azuga/src/test/java/browserTest/TestingLaunch.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestingLaunch {
 	public static void main(String[] args) {
+		System.out.println("Base");
 		System.setProperty("webdriver.chrome.driver", "chromedriver");
 		
 		WebDriver driver=new ChromeDriver();
